a1fc4a365d3f5cb8c0102e69e780426113bd0439
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-16 20:21:38 +0800

    define args structure

---
+++ src/args.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+use clap::Subcommand;
+use clap::ValueEnum;
+
+#[derive(Parser)]
+#[command(author, about, next_line_help = true)]
+pub struct Args {
+	#[command(subcommand)]
+	action: Actions
+}
+
+#[derive(Subcommand)]
+pub enum Actions {
+	Stage {
+		#[arg(value_enum)]
+		which: Stageable
+	},
+	Unstage {
+		#[arg(value_enum)]
+		which: UnStageable
+	}
+}
+
+#[derive(ValueEnum, Clone, Copy)]
+pub enum Stageable {
+
+}
+
+#[derive(ValueEnum, Clone, Copy)]
+pub enum UnStageable {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-mod parser;
+use crate::args::Args;
+use crate::parser::Stats;
+use clap::Parser;
+
+mod args;
 mod git;
+mod parser;
 
 fn main() {
-	let git_status = parser::Stats::new_from_git_status();
+	let args = Args::parse();
+	let git_status = Stats::new_from_git_status();
 	println!("{:#?}", git_status)
 }
