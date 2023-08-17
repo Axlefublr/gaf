def77987697fe1316e0ce553ffe955227059ebf8
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-08-17 17:55:51 +0800

    impl regex parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,32 +1,34 @@
+use regex::Regex;
+
 use crate::git;
 
 #[derive(Debug)]
 pub struct Stats {
-	pub unstaged: usize,
-	pub added: usize,
-	pub staged: usize,
-	pub modified: usize,
-	pub renamed: usize,
-	pub deleted: usize,
-	pub staged_deleted: usize,
+	pub unstaged: Vec<String>,
+	pub added: Vec<String>,
+	pub staged_modifications: Vec<String>,
+	pub unstaged_modifications: Vec<String>,
+	pub renamed: Vec<String>,
+	pub unstaged_deletions: Vec<String>,
+	pub staged_deletions: Vec<String>,
 }
 
 impl Stats {
 	fn new() -> Self {
 		Self {
-			unstaged: 0,
-			added: 0,
-			staged: 0,
-			modified: 0,
-			renamed: 0,
-			deleted: 0,
-			staged_deleted: 0,
+			unstaged: vec![],
+			added: vec![],
+			staged_modifications: vec![],
+			unstaged_modifications: vec![],
+			renamed: vec![],
+			unstaged_deletions: vec![],
+			staged_deletions: vec![],
 		}
 	}
 	pub fn new_from_git_status() -> Option<Self> {
 		let mut stats = Stats::new();
 		parse_status(&mut stats)?;
-		if are_all_zero(&stats) {
+		if are_all_empty(&stats) {
 			None
 		} else {
 			Some(stats)
@@ -36,36 +38,37 @@ impl Stats {
 
 fn parse_status(stats: &mut Stats) -> Option<()> {
 	let status = git::status()?;
-	for line in status.lines() {
-		let mut chars = line.chars();
-		let first = chars.next()?;
-		match first {
-			'M' => stats.staged += 1,
-			'A' => stats.added += 1,
-			'D' => stats.staged_deleted += 1,
-			'R' => stats.renamed += 1,
-			'?' => {
-				stats.unstaged += 1;
-				continue;
+	let regex = Regex::new(r"^(.{2}) ([^(?: \->)\n]*)(?: -> )?(.*)?$").unwrap();
+	let captures = regex.captures_iter(&status);
+	for capture in captures {
+		let path = capture[3].to_owned();
+		match &capture[1] {
+			"M" => stats.staged_modifications.push(path.clone()),
+			"A" => stats.added.push(path.clone()),
+			"D" => stats.staged_deletions.push(path.clone()),
+			"R" => {
+				stats.renamed.push(path.clone());
+				let staged_deletion = capture[4].to_owned();
+				stats.renamed.push(staged_deletion)
 			}
-			_ => (),
+			"?" => stats.unstaged.push(path.clone()),
+			&_ => (),
 		}
-		let second = chars.next()?;
-		match second {
-			'M' => stats.modified += 1,
-			'D' => stats.deleted += 1,
-			_ => (),
+		match &capture[2] {
+			"M" => stats.unstaged_modifications.push(path),
+			"D" => stats.unstaged_deletions.push(path),
+			&_ => ()
 		}
 	}
 	Some(())
 }
 
-fn are_all_zero(stats: &Stats) -> bool {
-	stats.added == 0
-		&& stats.deleted == 0
-		&& stats.modified == 0
-		&& stats.renamed == 0
-		&& stats.staged == 0
-		&& stats.unstaged == 0
-		&& stats.staged_deleted == 0
+fn are_all_empty(stats: &Stats) -> bool {
+	stats.added.is_empty()
+		&& stats.unstaged_deletions.is_empty()
+		&& stats.unstaged_modifications.is_empty()
+		&& stats.renamed.is_empty()
+		&& stats.staged_modifications.is_empty()
+		&& stats.unstaged.is_empty()
+		&& stats.staged_deletions.is_empty()
 }
